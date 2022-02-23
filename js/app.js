3b773cb8b224ba11995f65a98eae209dd92b8f6d
albicorra <alberto2001corrado@gmail.com>
2022-02-23 15:32:24 +0100

    esercizio finito

--- js/app.js
+++ js/app.js
@@ -10,24 +10,38 @@ function innerNum(num, container) {
     container.innerHTML += `<div class="random-number fs-1">${num}</div>`;
 }
 
-function userNum(params) {
-    
-}
+
 
 let randomNumber;
 let randomNumberArray = [];
 const randomNumContainer = document.getElementById('num-random-container');
 const buttonStart = document.getElementById('button-start');
 const container = document.querySelector('.container');
+const userContainer = document.querySelector('.user-container');
 let timer = 0;
 let clock;
+const userInput1 = document.getElementById('user-number-1');
+const userInput2 = document.getElementById('user-number-2');
+const userInput3 = document.getElementById('user-number-3');
+const userInput4 = document.getElementById('user-number-4');
+const userInput5 = document.getElementById('user-number-5');
+const userInputArray = [userInput1, userInput2, userInput3, userInput4, userInput5]
+let userNumArray = [];
+const buttonCheck = document.getElementById('button-check');
 
-for (let i = 0; i < 5; i++) {
-    
+
+
+do {
+
     randomNumber = getRandomIntInclusive(1, 20);
-    randomNumberArray.push(randomNumber);
+
+    if (!randomNumberArray.includes(randomNumber)) {   
+        randomNumberArray.push(randomNumber);
+    }
     
-}
+} while (randomNumberArray.length < 5);
+
+
 
 console.log(randomNumberArray);
 
@@ -42,11 +56,41 @@ buttonStart.addEventListener('click', function(){
 
     container.classList.remove('active');
 
-    let clock = setInterval( fuction () {
-
+    clock = setTimeout(() => {
         
+        userContainer.classList.add('active');
 
-    }, 1000)
+        buttonCheck.addEventListener('click', function(){
+            let i = 0;
+
+            userInputArray.forEach(element => {
+                userNumArray.push(element.value)
+            });
+
+            
+
+
+
+            console.log(userNumArray)
+
+            if (userNumArray.includes(userInputArray[i].value)){   
+                
+                alert( 'Hai messo dei numeri uguali')
+
+            }
+            
+            if (parseInt(userNumArray) == parseInt(randomNumberArray)) {
+                alert('Bravo, li hai indovinati!')
+            }else{
+                alert('Mi dispiace, non li hai ricordati tutti :(')
+            }
+
+            userNumArray = []
+
+        })
+
+
+    }, 30* 1000);
 
 })
 
